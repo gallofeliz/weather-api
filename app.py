a127d4fa75fde2d8d8167649db5d35a8f62317b0
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-12-08 17:10:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from meteofrance_api import MeteoFranceClient
 import requests, time, datetime, dateutil.parser, socketserver, http.server, urllib.parse, json
 
-class SunriseSunset():
+"""
+  Weather Model (Providers provides what they can)
+  - timestamp:int - data time
+  - civilDaylight:boolean - if it's the day from civil twilights
+  - temperature:float - temperature °c 1 decimal
+  - humidity:int - humidity %
+  - wind:int - Wind km/h
+  - windGust:int - Wind Gust km/h
+  - cloudiness:int - Cloudiness %
+  - rain1h/rain3h/rain6h:float - rain for 1h or 3h or 6h in mm 1 decimal
+  - snow1h/snow3h/rain6h:float - like rain but for snow
+  - icon:string - a full url for an icon
+  - description: string - an human description
+"""
+
+class SunriseSunsetProvider():
     def current(self, latitude, longitude):
         resp = requests.get(
             'https://api.sunrise-sunset.org/json',
@@ -19,52 +34,119 @@ class SunriseSunset():
             'timestamp': ts,
             'civilDaylight': True if ts > civilSunrise and ts < civilSunset else False
         }
+    def forecast(self, latitude, longitude):
+        return [] # I need not ahah
 
-class MeteoFranceWeather():
+class MeteoFranceProvider():
+    client = MeteoFranceClient()
+
     def current(self, latitude, longitude):
-        client = MeteoFranceClient()
-        resp = client.session.request(
+        return self.__map(self.__call(latitude, longitude, 'current'), 'current')
+    def forecast(self, latitude, longitude):
+        resp = self.__call(latitude, longitude, 'forecast')['forecast']
+
+        values = []
+        ts = int(round(time.time()))
+
+        for hour_resp in resp:
+            if hour_resp['dt'] > ts:
+                values.append(self.__map(hour_resp, 'forecast'))
+
+        return values
+    def __map(self, resp, type):
+        return {
+            **{
+                'timestamp': resp['dt'],
+                'temperature': round(resp['T']['value'], 1),
+                'humidity': round(resp['humidity']),
+                'wind': round(resp['wind']['speed'] * 3.6),
+                'windGust': round(resp['wind']['gust'] * 3.6 if resp['wind']['gust'] != 0 else resp['wind']['speed'] * 3.6)
+                # No other for current ... ???
+            },
+            **({
+                # Values depends on forecast granularity ; putting None to "unset" previous values (TODO check if relevant with my influxdb)
+                'rain1h': round(resp['rain']['1h'], 1) if '1h' in resp['rain'] else None,
+                'snow1h': round(resp['snow']['1h'], 1) if '1h' in resp['snow'] else None,
+                'rain3h': round(resp['rain']['3h'], 1) if '3h' in resp['rain'] else None,
+                'snow3h': round(resp['snow']['3h'], 1) if '3h' in resp['snow'] else None,
+                'rain6h': round(resp['rain']['6h'], 1) if '6h' in resp['rain'] else None,
+                'snow6h': round(resp['snow']['6h'], 1) if '6h' in resp['snow'] else None,
+                'cloudiness': round(resp['clouds']),
+                'icon': 'https://meteofrance.com/modules/custom/mf_tools_common_theme_public/svg/weather/%s.svg' % resp['weather']['icon'],
+                'description': resp['weather']['desc']
+            } if type == 'forecast' else {})
+        }
+    def __call(self, latitude, longitude, type):
+        resp = self.client.session.request(
             'get',
-            'current',
+            type,
             params={'lat': latitude, 'lon': longitude, 'lang': 'fr'},
         ).json()
 
         print('meteofrance current api response', resp)
 
-        return {
-            'timestamp': resp['dt'],
-            'temperature': round(resp['T']['value'], 1),
-            'humidity': round(resp['humidity']),
-            'wind': round(resp['wind']['speed'] * 3.6),
-            'windGust': round(resp['wind']['gust'] * 3.6)
-            # No other ...
-        }
+        return resp
 
-class OpenWeatherMap():
+
+class OpenWeatherMapProvider():
+    def __init__(self, appid):
+        self.appid = appid
+
     def current(self, latitude, longitude):
+        return self.__map(self.__call(latitude, longitude, 'current'), 'current')
+    def forecast(self, latitude, longitude):
+        resp = self.__call(latitude, longitude, 'forecast')
+
+        values = []
+
+        for hour_resp in resp['list']:
+            values.append(self.__map(hour_resp, 'forecast'))
+
+        return values
+    def __map(self, resp, type):
+        return {
+            **{
+                'timestamp': resp['dt'],
+                'temperature': round(resp['main']['temp'], 1),
+                'humidity': round(resp['main']['humidity']),
+                'wind': round(resp['wind']['speed'] * 3.6),
+                'windGust': round(resp['wind']['gust'] * 3.6 if 'gust' in resp['wind'] else resp['wind']['speed'] * 3.6),
+                'cloudiness': round(resp['clouds']['all']),
+                'icon': 'https://openweathermap.org/img/wn/%s@2x.png' % resp['weather'][0]['icon'],
+                'description': resp['weather'][0]['description']
+            },
+            **(
+                # Missing value equals to 0
+                {
+                    'rain3h': round(resp['rain']['3h'], 1) if 'rain' in resp and '3h' in resp['rain'] else 0.00,
+                    'snow3h': round(resp['snow']['3h'], 1) if 'snow' in resp and '3h' in resp['snow'] else 0.00
+                }
+                if type == 'forecast' else
+                {
+                    'rain1h': round(resp['rain']['1h'], 1) if 'rain' in resp and '1h' in resp['rain'] else 0.00,
+                    'snow1h': round(resp['snow']['1h'], 1) if 'snow' in resp and '1h' in resp['snow'] else 0.00
+                }
+            )
+        }
+    def __call(self, latitude, longitude, type):
         resp = requests.get(
-            'http://api.openweathermap.org/data/2.5/weather',
-            params={'appid': 'xxx', 'units': 'metric', 'lat': latitude, 'lon': longitude, 'lang': 'fr'}
+            'http://api.openweathermap.org/data/2.5/' + ('weather' if type == 'current' else 'forecast'),
+            params={'appid': self.appid, 'units': 'metric', 'lat': latitude, 'lon': longitude, 'lang': 'fr'}
         ).json()
 
         print('openweathermap current api response', resp)
 
-        return {
-            'timestamp': resp['dt'],
-            'temperature': round(resp['main']['temp'], 1),
-            'humidity': round(resp['main']['humidity']),
-            'wind': round(resp['wind']['speed'] * 3.6),
-            'windGust': round(resp['wind']['gust'] * 3.6 if 'gust' in resp['wind'] else resp['wind']['speed'] * 3.6),
-            'cloudiness': round(resp['clouds']['all']),
-            'rain1h': round(resp['rain']['1h'], 2) if 'rain' in resp and '1h' in resp['rain'] else 0.00,
-            'snow1h': round(resp['snow']['1h'], 2) if 'snow' in resp and '1h' in resp['snow'] else 0.00,
-            'icon': 'http://openweathermap.org/img/wn/%s@2x.png' % resp['weather'][0]['icon'],
-            'desc': resp['weather'][0]['description']
-        }
+        return resp
 
-class Weather():
+class WeatherService():
     def __init__(self, providers):
         self.providers = providers
+
+    def get_current(self, latitude, longitude, location_alias):
+        return self.__read('current', latitude, longitude, location_alias)
+
+    def get_forecast(self, latitude, longitude, location_alias):
+        return self.__read('forecast', latitude, longitude, location_alias)
 
     # use of position object instead of three args ?
     def __read(self, type, latitude, longitude, location_alias):
@@ -75,7 +157,7 @@ class Weather():
           try:
             values[providerName] = getattr(self.providers[providerName], type)(latitude, longitude)
           except Exception as e:
-            print(e)
+            print(str(e))
             errors.append(providerName)
 
         return {
@@ -89,17 +171,10 @@ class Weather():
           'errors': errors
         }
 
-    def get_current(self, latitude, longitude, location_alias):
-        return self.__read('current', latitude, longitude, location_alias)
-
-    def get_forecast(self, latitude, longitude, location_alias):
-        return self.__read('forecast', latitude, longitude, location_alias)
-
-
-weather = Weather({
-    'meteoFrance': MeteoFranceWeather(),
-    'openWeatherMap': OpenWeatherMap(),
-    'sunriseSunset': SunriseSunset()
+weather = WeatherService({
+    'meteoFrance': MeteoFranceProvider(),
+    'openWeatherMap': OpenWeatherMapProvider('xxx'),
+    'sunriseSunset': SunriseSunsetProvider()
 })
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -122,7 +197,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             if sMac == 'current':
                 data = weather.get_current(49.1349243, 1.4400054, 'home')
             else:
-                data = weather.get_current(49.1349243, 1.4400054, 'home')
+                data = weather.get_forecast(49.1349243, 1.4400054, 'home')
             self.send_response(200)
             self.send_header('Content-type','application/json')
             self.end_headers()
@@ -134,7 +209,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(bytes(str(inst), 'utf8'))
             print('ERROR ' + str(inst))
 
-httpd = socketserver.TCPServer(('', 8081), Handler)
+httpd = socketserver.TCPServer(('', 8080), Handler)
 try:
    print('Listening')
    httpd.serve_forever()
